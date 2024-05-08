a66bbce7ff5cf79222ecf75e0f95fb80c3984704
timetowinanother <spotch@gmail.com>
2024-05-07 20:26:34 -0400

    Update emojify.py

--- emojify.py
+++ emojify.py
@@ -1,10 +1,31 @@
-s=input("What do you want to emojify?: ")
+s="1"
 
-for c in s:
-        if c.isdigit() or c.isalpha(): print(":"+c+":", end="")
-        elif c == " ": print("    ", end="")
-        elif c == "!": print(":exclamation:", end="")
-        elif c == "?": print(":question:", end="")
-        else: print(c, end="")
+def randomletters(s):
+        for c in s:
+                if c.isalpha(): print(":"+c+":", end="")
+                elif c.isdigit(): print(":e"+c+":", end="")
+                elif c == " ": print("    ", end="")
+                elif c == "!": print(":exclamation:", end="")
+                elif c == "?": print(":question:", end="")
+                else: print(c, end="")
+        print()
 
-print()
+
+def whiteletters(s):
+        for c in s:
+                if c.isalpha(): print(":alphabet-white-"+c+":", end="")
+                elif c.isdigit(): print(":e"+c+":", end="")
+                elif c == " ": print("    ", end="")
+                elif c == "!": print(":exclamation:", end="")
+                elif c == "?": print(":question:", end="")
+                else: print(c, end="")
+        print()
+
+
+while s:
+        s=input("What characters  would you like to emojify?: ")
+        if s == "":
+                 break
+        choice=input("Enter '1' for random letters and '2' for white letters: ")
+        if choice == "2": whiteletters(s)
+        else: randomletters(s)
