a82a33aca78d33bec7aa85d8e36b5c49cadddc4d
timetowinanother <spotch@gmail.com>
2024-05-09 18:48:00 -0400

    Update emojify.js

--- emojify.js
+++ emojify.js
@@ -1,4 +1,4 @@
-//Paste the javascript url below into the URL bar of a bookmark, and when you click on that bookmark it will prompt you to enter the words you want to emojify. 
-//Because you can only highlight an Alert's response when it is one line or less, for longer strings you will need to open the developer tools window, go to console, and copy the output from the console. 
+//Paste the javascript url below into the URL bar of a bookmark, and when you click on that bookmark it will prompt you to enter the words you want to emojify.
+//See the Read Me file for implementation instructions and limitations in Chrome.
 
 javascript: function main() {var str=prompt("What would you like to emojify?"); var final=""; var letters = /^[a-zA-Z]*$/; var numbers = /^[0-9]*$/; for (let i = 0; i < str.length; i++) {if (str.charAt(i).match(letters)) {final=final.concat(":"+str.charAt(i)+":")} else if (str.charAt(i).match(numbers)) {final=final.concat(":e"+str.charAt(i)+":")} else if (str.charAt(i) == " " || str.charAt(i) ==".") {final=final.concat(str.charAt(i)+"   ")} else if (str.charAt(i) == "?") {final=final.concat(":question:")} else if (str.charAt(i) == "!") {final=final.concat(":exclamation:")} else {final=final.concat(str.charAt(i))}}; console.log(final); window.alert(final); } main()
