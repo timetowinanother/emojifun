52a805e8dec28ae52ac91957dde0c7f1189f752c
timetowinanother <spotch@gmail.com>
2024-05-10 20:48:51 -0400

    Update emojify.py
    made the code more user friendly, white exclamation and question, added more spaces between words.

--- emojify.py
+++ emojify.py
@@ -4,7 +4,7 @@ def randomletters(s):
     for c in s:
         if c.isalpha(): print(":"+c+":", end="")
         elif c.isdigit(): print(":e"+c+":", end="")
-        elif c == " ": print("    ", end="")
+        elif c == " ": print("     ", end="")
         elif c == "!": print(":exclamation:", end="")
         elif c == "?": print(":question:", end="")
         else: print(c, end="")
@@ -15,17 +15,17 @@ def whiteletters(s):
     for c in s:
         if c.isalpha(): print(":alphabet-white-"+c+":", end="")
         elif c.isdigit(): print(":e"+c+":", end="")
-        elif c == " ": print("    ", end="")
-        elif c == "!": print(":exclamation:", end="")
-        elif c == "?": print(":question:", end="")
+        elif c == " ": print("     ", end="")
+        elif c == "!": print(":alphabet-white-exclamation:", end="")
+        elif c == "?": print(":alphabet-white-question:", end="")
         else: print(c, end="")
     print()
 
 
 while s:
-    s=input("What characters  would you like to emojify?: ")
+    s=input("What characters  would you like to emojify? (or press <Enter> to quit): ")
     if s == "":
         break
-    choice=input("Enter '1' for random letters and '2' for white letters: ")
-    if choice == "2": whiteletters(s)
+    choice=input("Enter '1' for white letters or any other key for random letters: ")
+    if choice == "1": whiteletters(s)
     else: randomletters(s)
